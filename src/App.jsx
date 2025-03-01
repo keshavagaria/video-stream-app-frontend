63a0d7e880447791dc2a2f7c13460e55d33f8978
Vishal Singh <vishal267s@gmail.com>
2025-03-01 19:53:35 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,71 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 import VideoUpload from './components/VideoUpload'
 import { Toaster } from 'react-hot-toast'
+import VideoPlayer from "./components/VideoPlayer"
+import { Button, TextInput } from "flowbite-react"
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [fieldValue, setFieldValue] = useState(null);
   const [videoId, setVideoId] =useState("03eb135a-3516-4e4a-8c2c-521df3b33279");
 
   return (
     
     
-      <div className='flex flex-col space-x-10 space-y-10' >
-        
-        <div className='text-center'>
-          <h1 className='text-5xl font-extrabold dark:text-gray-100'>Video Streaming App</h1>
+    <>
+    <Toaster />
+    <div className="flex flex-col  items-center space-y-9 justify-center py-9">
+      <h1 className="text-2xl font-bold text-gray-700 dark:text-gray-100">
+        Video Streaming App
+      </h1>
+
+      <div className="flex mt-14 w-full space-x-2  justify-between">
+        <div className="w-full">
+          <h1 className="text-white text-center mt-2">Playing Video</h1>
+
+        <div>
+          <video style={{
+                width: "100%",
+              }}
+          src={`http://localhost:8080/api/v1/videos/stream/${videoId}`}
+          controls>
+        </video> 
+      </div>
+          {/* <div>
+            <VideoPlayer
+              src="{`http://localhost:8080/api/v1/videos/${videoId}`}"
+            ></VideoPlayer>
+          </div> */}
+
+  
+        </div>
+
+        <div className="w-full">
+          <VideoUpload />
+        </div>
       </div>
 
- 
-      <div className='flex flex-row space-x-10 py-10'>
-          <div className='items-start'>
-            <h1 className='text-white'>Playing Video</h1>
-            <video style={{width:500}} 
-            src={`http://localhost:8080/api/v1/videos/stream/range/${videoId}`}
-            controls></video>
-
-{/* <video
-    id="my-video"
-    class="video-js"
-    controls
-    preload="auto"
-    width="640"
-    height="264"
-    data-setup="{}"
-  >
-    <source src={`http://localhost:8080/api/v1/videos/stream/range/${videoId}`} type="video/mp4" />
-    <p class="vjs-no-js">
-      To view this video please enable JavaScript, and consider upgrading to a
-      web browser that
-      <a href="https://videojs.com/html5-video-support/" target="_blank"
-        >supports HTML5 video</a
-      >
-    </p>
-  </video> */}
-        </div>
-        
-        <div className='items-end'>
-          <VideoUpload />
-         <Toaster></Toaster>
-       </div>
-    </div>  
+      <div className="my-4 flex  space-x-4">
+        <TextInput
+          onClick={(event) => {
+            setFieldValue(event.target.value);
+          }}
+          placeholder="Enter video id here"
+          name="video_id_field"
+        />
+        <Button
+          onClick={() => {
+            setVideoId(fieldValue);
+          }}
+        >
+          Play
+        </Button>
+      </div>
     </div>
+  </>
   )
 }
 
---
+++ src/components/VideoPlayer.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useRef } from "react"
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+import toast from "react-hot-toast";
+
+
+function VideoPlayer({ src }){
+
+    const videoRef=useRef(null);
+    const playerRef=useRef(null);
+
+
+    useEffect(()=>{
+      playerRef.current =  videojs(videoRef.current,{
+            controls:true,
+            autoplay:true,
+            muted:true,
+            preload:"auto",
+            download:true,
+        });
+        
+
+        // if (Hls.isSupported()) {
+        //     const hls = new Hls();
+        //     hls.loadSource(src);
+        //     hls.attachMedia(videoRef.current);
+        //     hls.on(Hls.Events.MANIFEST_PARSED, () => {
+        //       videoRef.current.play();
+        //     });
+        //   } else if (videoRef.current.canPlayType("application/vnd.apple.mpegurl")) {
+        //     videoRef.current.src = src;
+        //     videoRef.current.addEventListener("canplay", () => {
+        //       videoRef.current.play();
+        //     });
+        //   } else {
+        //     console.log("video format not supportted");
+        //     toast.error("Video format not supporteds");
+        //   }
+
+    },[src]);
+    return (
+        <div>
+        <div data-vjs-player>
+          <video
+            ref={videoRef}
+            style={{
+              width: "100%",
+              height: "500px",
+            }}
+            className="video-js vjs-control-bar"
+            controls
+          ></video>
+        </div>
+      </div>
+    )
+}
+
+
+export default VideoPlayer
